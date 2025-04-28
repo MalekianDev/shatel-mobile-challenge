40f9197ad78d775fa5253d77e84f69b156b5c668
samanmalekian <malekian@protonmail.com>
2025-04-28 19:11:14 +0330

    - Setup postgres DB
    - Added rest_framework to apps list
    
    - Added secrets example file
    
    - Regenarated secret_key

--- backend/_core/settings.py
+++ backend/_core/settings.py
@@ -10,22 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load enviroment variables from secrets.env
+load_dotenv(BASE_DIR / "secrets.env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-lse$2x$h_(a+&*ufgv22-)^1d*mu*agy4ccnhu3n17rj+h7lr$"
+SECRET_KEY = os.getenv("SECRET_KEY", "1")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv("DEBUG", default=0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default="127.0.0.1 localhost").split(" ")
 
 
 # Application definition
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -73,10 +79,15 @@ WSGI_APPLICATION = "_core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
+        "NAME": os.getenv("DB_NAME", default="test_db"),
+        "USER": os.getenv("DB_USER", default="test_user"),
+        "PASSWORD": os.getenv("DB_PASSWORD", default="1234"),
+        "HOST": os.getenv("DB_HOST", default="localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", default="5432"),
     }
 }
 
