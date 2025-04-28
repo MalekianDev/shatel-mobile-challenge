63b9feae9ff92da1f7e2b4186e949c9b0afa8b1c
samanmalekian <malekian@protonmail.com>
2025-04-28 20:12:26 +0330

    - Customized User model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,1 +1,11 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """
+    Porject document mentioned: send emails to all registered users using a dynamic email template
+    I override User model email field to make it unique & not blank
+    """
+
+    email = models.EmailField(unique=True)
