1d23b4202f990551674301d53e06fca999039b02
samanmalekian <malekian@protonmail.com>
2025-04-28 18:08:02 +0330

    - Restructured the Django default main app

--- backend/_core/settings.py
+++ backend/_core/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "backend.urls"
+ROOT_URLCONF = "_core.urls"
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "backend.wsgi.application"
+WSGI_APPLICATION = "_core.wsgi.application"
 
 
 # Database
