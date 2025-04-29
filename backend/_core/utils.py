de432855f53b38500cb6609fe9384ca0f0d65f99
samanmalekian <malekian@protonmail.com>
2025-04-30 02:25:33 +0330

    - Added send_email function for tests simplicity

---
+++ backend/_core/utils.py
@@ -0,0 +1,7 @@
+def send_email(to, subject, message):
+    """
+    Simple example function instead of "django.core.mail.send_mail" for simplicity.
+    Mentioned in document:
+    Please note that the implementation of the "send email" functionality is not required for this challenge.
+    """
+    print(f"Email sent to {to} with subject {subject} and message {message}")
