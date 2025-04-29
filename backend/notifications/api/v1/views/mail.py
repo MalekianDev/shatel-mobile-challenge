1c956a7124353ca4214dd508ba45625f21aeddde
samanmalekian <malekian@protonmail.com>
2025-04-30 00:55:57 +0330

    - Improved mail view's queries using select_related & prefetch_related

--- backend/notifications/api/v1/views/mail.py
+++ backend/notifications/api/v1/views/mail.py
@@ -5,20 +5,20 @@ from notifications.models import MailBulk, MailTemplate
 
 
 class MailBulkListCreateAPIView(ListCreateAPIView):
-    queryset = MailBulk.objects.all()
+    queryset = MailBulk.objects.all().select_related("template", "creator", "detail")
     serializer_class = MailBulkSerializer
 
 
 class MailBulkRetrieveUpdateAPIView(RetrieveUpdateAPIView):
-    queryset = MailBulk.objects.all()
+    queryset = MailBulk.objects.all().select_related("template", "creator", "detail")
     serializer_class = MailBulkSerializer
 
 
 class MailTemplateListCreateAPIView(ListCreateAPIView):
-    queryset = MailTemplate.objects.all()
+    queryset = MailTemplate.objects.all().select_related("creator")
     serializer_class = MailTemplateSerializer
 
 
 class MailTemplateRetrieveUpdateAPIView(RetrieveUpdateAPIView):
-    queryset = MailTemplate.objects.all()
+    queryset = MailTemplate.objects.all().select_related("creator").prefetch_related("bulk_mails")
     serializer_class = MailTemplateSerializer
