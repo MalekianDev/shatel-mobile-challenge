7b35de971f3b080fec038401c1be249f07cb53e0
samanmalekian <malekian@protonmail.com>
2025-04-30 00:11:35 +0330

    - Added mail template list/create/retrieve/update endpoints

--- backend/notifications/api/v1/views/__init__.py
+++ backend/notifications/api/v1/views/__init__.py
@@ -1,6 +1,4 @@
-from .mail import (  # noqa
-    MailBulkListCreateAPIView,
-    MailBulkRetrieveUpdateAPIView,
-    MailTemplateListCreateAPIView,
-    MailTemplateRetrieveUpdateAPIView,
-)
+from .mail import MailBulkListCreateAPIView  # noqa
+from .mail import MailBulkRetrieveUpdateAPIView  # noqa
+from .mail import MailTemplateListCreateAPIView  # noqa
+from .mail import MailTemplateRetrieveUpdateAPIView  # noqa
