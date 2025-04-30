a066b0c510de186c4277786dbaa3e012c8b1d579
samanmalekian <malekian@protonmail.com>
2025-04-30 11:24:09 +0330

    - Added a detail explation comment during review

--- backend/_core/validators.py
+++ backend/_core/validators.py
@@ -2,5 +2,6 @@ from django.core.exceptions import ValidationError
 
 
 def validate_csv_file(value: str) -> None:
+    # Also its good to check file content type
     if not value.name.endswith(".csv"):
         raise ValidationError("Only CSV files are allowed.")
