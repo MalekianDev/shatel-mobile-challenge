7c464dc00ead82b732bc0d5a32e882e7fe618349
samanmalekian <malekian@protonmail.com>
2025-05-01 18:04:28 +0330

    - Fixed some naming issues

--- frontend/src/pages/email-bulk/MailBulkDetail.jsx
+++ frontend/src/pages/email-bulk/MailBulkDetail.jsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 
-export default function MailTemplate() {
+export default function MailBulkDetail() {
     return (
         <Box sx={{ width: '60%', margin: 'auto', padding: '50px' }}>
             <h1>Mail Bulk Detail</h1>
